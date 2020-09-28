144a1ae6ff286353509f9554de5c941e682ce2b0
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-09-28 12:42:44 -0400

    created DuringLeave, blank though

---
+++ LCS Toolkit/duringLeave.swift
@@ -0,0 +1,20 @@
+//
+//  duringLeave.swift
+//  LCS Toolkit
+//
+//  Created by Cloutier, Vincent on 2020-09-28.
+//
+
+import SwiftUI
+
+struct duringLeave: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct duringLeave_Previews: PreviewProvider {
+    static var previews: some View {
+        duringLeave()
+    }
+}
