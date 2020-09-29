d2353ed0e79cff1d78dbca20ef2e4398ded6b177
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-09-28 20:38:07 -0400

    really rudimentary timer set up

--- LCS Toolkit/duringLeave.swift
+++ LCS Toolkit/duringLeave.swift
@@ -10,20 +10,29 @@ import SwiftUI
 struct duringLeave: View {
     @State var username: String = ""
     @State var password: String = ""
-    
+    @State var currentDate = Date()
+    @State var timeRemaining = 7200
+
     var body: some View {
         NavigationView {
             Form {
                 Section {
-                    //timer here
-                    }
-                Button(action: {
-                                // activate theme!
-                                }) {
-                                    Text("Check In")
+                    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+
+                        Text("\(timeRemaining)")
+                            .onReceive(timer) { _ in
+                                if self.timeRemaining > 0 {
+                                    self.timeRemaining -= 1
                                 }
+                            }
+                                        }
+                Button(action: {
+                    // activate theme!
+                }) {
+                    Text("Check In")
+                }
             }
-        .navigationTitle("During Leave")
+            .navigationTitle("During Leave")
         }
     }
 }
@@ -36,4 +45,8 @@ struct duringLeave_Previews: PreviewProvider {
 }
 
 
+
+
+    
+    
 
